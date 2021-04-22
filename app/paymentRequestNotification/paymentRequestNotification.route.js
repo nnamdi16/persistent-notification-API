7fdc422081617d085ac62f9f2de9d218c9bea33f
Nnamdi <nnwabuokei@paga.com>
2021-04-22 16:46:39 +0100

    Added payment request method

--- app/paymentRequestNotification/paymentRequestNotification.route.js
+++ app/paymentRequestNotification/paymentRequestNotification.route.js
@@ -3,6 +3,6 @@ const router = Router();
 const {receiveNotification, basicAuth} = require("./paymentRequestNotification.controller");
 
 
-router.route("/paymentRequest").post(basicAuth, receiveNotification);
+router.route("/paymentrequest").post(basicAuth, receiveNotification);
 
 module.exports = router;

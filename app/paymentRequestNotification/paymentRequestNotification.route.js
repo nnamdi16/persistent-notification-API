211d43498d3a003ef04717b3db09ef5c76fb9750
Nnamdi <nnwabuokei@paga.com>
2021-04-22 16:41:20 +0100

    modified the route

--- app/paymentRequestNotification/paymentRequestNotification.route.js
+++ app/paymentRequestNotification/paymentRequestNotification.route.js
@@ -3,6 +3,6 @@ const router = Router();
 const {receiveNotification, basicAuth} = require("./paymentRequestNotification.controller");
 
 
-router.route("/paymentRequest/notify").post(basicAuth, receiveNotification);
+router.route("/paymentRequest").post(basicAuth, receiveNotification);
 
 module.exports = router;

cdd8437aa58ea2c2b626968c49e2632ad868fad1
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-08-04 13:22:23 +0100

    chore:refactored the service to accomodate the modified model

--- app/paymentRequestNotification/paymentRequestNotification.service.js
+++ app/paymentRequestNotification/paymentRequestNotification.service.js
@@ -1,37 +1,51 @@
+/* eslint-disable prettier/prettier */
 const PaymentRequestNotificationSchema = require("./paymentRequestNotification.model");
-const {info} = require("heroku-log");
+const { info } = require("heroku-log");
 
 exports.receiveNotification = async function(data) {
-  try {
-   console.log(data);
-    const {
-      referenceNumber,
-      statusCode,
-      statusMessage,
-      paymentMethods,
-      expiryDateTimeUTC,
-      payerPagaAccountHolder
-      
-    } = data;
+    try {
+        console.log(data);
+        const {
+            transactionReference,
+            statusCode,
+            statusMessage,
+            fundingPaymentReference,
+            accountNumber,
+            accountName,
+            amount,
+            financialIdentificationNumber,
+            clearingFeeAmount,
+            transferFeeAmount,
+            transferBankName,
+            transferBankAccountNumber,
+            hash
 
-    
-    const newNotification = new PaymentRequestNotificationSchema({
-      referenceNumber,
-      statusCode,
-      statusMessage,
-      paymentMethods,
-      expiryDateTimeUTC,
-      payerPagaAccountHolder
-    });
-    console.log(newNotification);
+        } = data;
 
-    await newNotification.save();
-    return {
-      status: false
-    };
-  } catch (error) {
-    throw new Error(error);
-  }
+
+        const newNotification = new PaymentRequestNotificationSchema({
+            transactionReference,
+            statusCode,
+            statusMessage,
+            fundingPaymentReference,
+            accountNumber,
+            accountName,
+            amount,
+            financialIdentificationNumber,
+            clearingFeeAmount,
+            transferFeeAmount,
+            transferBankName,
+            transferBankAccountNumber,
+            hash
+
+        });
+        console.log(newNotification);
+
+        await newNotification.save();
+        return {
+            status: true
+        };
+    } catch (error) {
+        throw new Error(error);
+    }
 };
-
-

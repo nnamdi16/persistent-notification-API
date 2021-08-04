34a1470e2d80e14237ac4d0b1a0df8bee6259c4e
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-08-04 13:21:21 +0100

    chore: modified the model

--- app/paymentRequestNotification/paymentRequestNotification.model.js
+++ app/paymentRequestNotification/paymentRequestNotification.model.js
@@ -1,10 +1,11 @@
+/* eslint-disable prettier/prettier */
 /**
  * Module dependencies
  */
 
 const {
-  Schema,
-  model
+    Schema,
+    model
 } = require("mongoose");
 
 /**
@@ -12,30 +13,49 @@ const {
  */
 
 let PaymentRequestNotificationSchema = new Schema({
-  referenceNumber: {
-    type: String
-  },
-  statusCode: {
-    type: String
-  },
-  statusMessage: {
-    type: String
-  },
-
-  paymentMethods: {
-    type: Array
-  },
-  
-  expiryDateTimeUTC: {
-    type: String,
-    default:null
-  },
-  payerPagaAccountHolder: {
-    type: Boolean
-  }
+    trnasctionReference: {
+        type: String
+    },
+    statusCode: {
+        type: String
+    },
+    statusMessage: {
+        type: String
+    },
+    fundingPaymentReference: {
+        type: String,
+        default: null
+    },
+    accountNumber: {
+        type: String
+    },
+    accountName: {
+        type: String
+    },
+    amount: {
+        type: Number
+    },
+    financialIdentificationNumber: {
+        type: String
+    },
+    clearingFeeAmount: {
+        type: Number
+    },
+    transferFeeAmount: {
+        type: Number
+    },
+    transferBankName: {
+        type: String
+    },
+    transferBankAccountNumber: {
+        type: String
+    },
+    hash: {
+        type: String
+    }
 
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 

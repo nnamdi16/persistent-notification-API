a282396624ef2d85faa0549f52e2909a65406186
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-08-04 13:20:54 +0100

    chore:modified the code structure

--- app/paymentRequestNotification/paymentRequestNotification.controller.js
+++ app/paymentRequestNotification/paymentRequestNotification.controller.js
@@ -1,74 +1,73 @@
-const {
-  receiveNotification
-} = require("./paymentRequestNotification.service");
-const {authenticate} = require('../customer/customer.service');
+/* eslint-disable prettier/prettier */
+const { receiveNotification } = require("./paymentRequestNotification.service");
+const { authenticate } = require("../customer/customer.service");
 
-const {info} = require("heroku-log");
-const logger = require('heroku-logger');
+const { info } = require("heroku-log");
+const logger = require("heroku-logger");
 const validate = require("../shared/util");
-exports.receiveNotification = async (req, res) => {
-  try {
-    res.set("Content-Type", "application/json");
-    res.set("Accept", "application/json");
-    const value = validate(req.body);
-    info(req.body);
-    logger.info(value);
-    const data = await receiveNotification(value);
-    info(data);
-    if (data.error) {
-      return res.status(200).json({
-        status: 0,
-        message: data.msg
-      });
-    }
+exports.receiveNotification = async(req, res) => {
+    try {
+        res.set("Content-Type", "application/json");
+        res.set("Accept", "application/json");
+        const value = validate(req.body);
+        info(req.body);
+        logger.info(value);
+        const data = await receiveNotification(value);
+        info(data);
+        if (data.error) {
+            return res.status(200).json({
+                status: 0,
+                message: data.msg
+            });
+        }
 
-    //or writeHead method
-    return res
-      .status(201)
-      // .header("Authorization", token)
-      .send({
-        status:'SUCCESS'
-      });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
+        //or writeHead method
+        return (
+            res
+            .status(201)
+            // .header("Authorization", token)
+            .send({
+                status: "SUCCESS"
+            })
+        );
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 };
 
-
 exports.basicAuth = async function(req, res, next) {
-  //Check for the basic auth header
+    //Check for the basic auth header
 
-  console.log(req.headers);
-  if (
-    !req.headers.authorization ||
-    req.headers.authorization.indexOf("Basic") === -1
-  ) {
-    return res.status(401).json({
-      message: "Missing Authorization Header"
-    });
-  }
+    console.log(req.headers);
+    if (!req.headers.authorization ||
+        req.headers.authorization.indexOf("Basic") === -1
+    ) {
+        return res.status(401).json({
+            message: "Missing Authorization Header"
+        });
+    }
 
-  //Verify auth credentials
-  const base64Credentials = req.headers.authorization.split(" ")[1];
-  const credentials = Buffer.from(base64Credentials, "base64").toString(
-    "ascii"
-  );
+    //Verify auth credentials
+    const base64Credentials = req.headers.authorization.split(" ")[1];
+    const credentials = Buffer.from(base64Credentials, "base64").toString(
+        "ascii"
+    );
 
-  const [username, password] = credentials.split(":");
-  // console.log(username,password)
+    const [username, password] = credentials.split(":");
+    // console.log(username,password)
 
-  const customer = await authenticate(username, password);
-  console.log(customer.error);
-  if (customer.error) {
-    return res.status(401).json({
-      error: true,
-      message: "Invalid Authentication Credentials"
-    });
-  }
-  //Attach customer to the request object
-  // req.customer = customer;
-  next();
+    const customer = await authenticate(username, password);
+    console.log(customer.error);
+    if (customer.error) {
+        return res.status(401).json({
+            error: true,
+            message: "Invalid Authentication Credentials"
+        });
+    }
+    //Attach customer to the request object
+    // req.customer = customer;
+    next();
 };
